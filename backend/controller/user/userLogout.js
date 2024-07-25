7ffe8c773de66fe99fdcce891bd041605ad4c6d7
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-07-25 17:52:36 +0500

    first commit

---
+++ backend/controller/user/userLogout.js
@@ -0,0 +1,27 @@
+async function userLogout(req, res) {
+    try {
+
+        const tokenOption = {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None'
+        }
+
+        res.clearCookie("token", tokenOption)
+
+        res.json({
+            message: "Logged out successfully!",
+            error: false,
+            success: true,
+            data: []
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
+    }
+}
+
+module.exports = userLogout
